45f926b3923c2090eac5dbd028e67b6498336b67
Cleferson Dannylo <clefersonnascimento8@gmail.com>
2021-10-18 15:47:08 -0200

    corrigindo as variáveis de ambiente

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,4 +12,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 }
 
-initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+export const database = getDatabase(app)
+export const auth = getAuth()
